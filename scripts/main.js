745f9c6df4cd501e463763b64bfe849d20631757
chrisbrickey <cbrickey1@gmail.com>
2017-05-15 02:20:25 -0700

    added warning to go to chrisbrickey.com for real site

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 var myHeading = document.querySelector('h1');
 //textContent is a property (the text contained within) of the tag variable that we created in the previous line-heigh
 //so we're resetting the content of <h1></h1> to "Hello World"...which will override whatever is on the index.html within h1 tags
-myHeading.textContent = "chrisbrickey.com";
+myHeading.textContent = "chrisbrickey.io";
 
 
 //selecting everything on the page and assigning it to a var
